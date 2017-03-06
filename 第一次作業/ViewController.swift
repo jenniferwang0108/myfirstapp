99473b873613dfd32e3ec4629b7c0c056406eee8
Jennifer Wang <jenniferwang@Jennifers-MacBook-Air-3.local>
2017-03-06 11:51:48 +0800

    Initial Commit

---
+++ 第一次作業/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  第一次作業
+//
+//  Created by Jennifer Wang on 3/6/17.
+//  Copyright © 2017 Jennifer Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
